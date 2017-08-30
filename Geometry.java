c12ce7cda1d5bc879c91a893da7ea078d183f470
Ian Thompson <ianthompson@nicelion.com>
2017-08-30 09:44:20 -0400

    added my name

--- Geometry.java
+++ Geometry.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Geometry here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Ian Thompson
+ * @version 0.0.1
  */
 public class Geometry 
 {
