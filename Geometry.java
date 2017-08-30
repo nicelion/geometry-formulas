589ae75bd31f80ded6fb9461a4294e994eef11d6
Ian Thompson <ianthompson@nicelion.com>
2017-08-30 17:09:07 -0400

    Fixed rectangular prism surface area and added comments

--- Geometry.java
+++ Geometry.java
@@ -20,43 +20,73 @@ public class Geometry
         
         return area;
     }
-    
+    /**
+     * Returns the area for a rectangle
+     * @param length Length of rectangle
+     * @param width Width of rectangle
+     */
     public static double rectangleArea(double length, double width) 
     {
         return length * width;
     }
-    
+   /**
+     * Returns the volume for a sphere
+     * @param radius Radius of sphere
+     */
     public static double sphereVolume(double radius) 
     {
         return 4.0 / 3.0 * Math.PI * Math.pow(radius, 3);
     }
-    
+    /**
+     * Returns the area for a parallelogram
+     * @param base Base of parallelogram
+     * @param height Height of parallelogram
+     */    
     public static double parallelogramArea(double base, double height)
     {
         double area = base * height;
         return area;
     }
-    
+    /**
+     * Returns the area for a trapezoid
+     * @param a Length side A of trapezoid
+     * @param b Length side B of zrapezoid
+     * @param height Height of trapezoid
+     */    
     public static double trapezoidArea(double a, double b, double height)
     {
         double area = ((a + b) / 2.0) * height;
         return area;
     }
-    
+    /**
+     * Returns the bolume for a rectangular prism
+     * @param w Length of rectangle
+     * @param h Width of rectangle
+     */    
     public static double rectangularPrismVolume(double w, double h, double length)
     {
         double volume = w * h * length;
         return volume;
     }
-    
+     /**
+     * Returns the area for a rectangle
+     * @param length Length of rectangle
+     * @param width Width of rectangle
+     */   
     public static double coneVolume(double r, double h)
     {
         double volume = (Math.PI * Math.pow(r, 2)) * (h / 3.0);
         return volume;
+
     }
+   /**
+     * Returns the area for a rectangle
+     * @param length Length of rectangle
+     * @param width Width of rectangle
+     */
     public static double rectPrismSurfaceArea(double length, double w, double h)
     {
-        double area = 2.0 * (w * length) + (h * length) + (h * w);
+        double area = 2.0 *((w * length) + (h * length) + (h * w));
         return area;
     }
     public static void main(String[] args) 
